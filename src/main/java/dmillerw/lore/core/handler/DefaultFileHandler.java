75b3bbc3e64fb5d7d7a55f80efd16ff94f5714b5
dmillerw <dmillerw@gmail.com>
2014-06-06 18:37:08 -0700

    Forgot that we don't actually need that file

--- src/main/java/dmillerw/lore/core/handler/DefaultFileHandler.java
+++ src/main/java/dmillerw/lore/core/handler/DefaultFileHandler.java
@@ -14,7 +14,6 @@ import java.net.URL;
 public class DefaultFileHandler {
 
 	public static final String[] FILES = new String[] {
-		"tags.json",
 		"lore/tutorial.json",
 		"lore/audio/tutorial.ogg"
 	};
